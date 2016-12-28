694a52c926731376dde232dedec479d1a64f669a
Fred Souza <fredericoamsouza@gmail.com>
2016-12-28 00:21:26 -0200

    Refactoring promise middleware

--- src/root.js
+++ src/root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import configureStore from './configure/store';
+import configureStore from './store';
 import App from './app';
 
 export default class Root extends Component {
--- src/store.js
+++ src/store.js
@@ -3,11 +3,23 @@ import thunk from 'redux-thunk';
 import reducer from '../reducers';
 import { persistStore } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
-import promise from './promise';
+
+function warn(error) {
+  console.warn(error.message || error);
+  throw error; // To let the caller handle the rejection
+}
+
+function resolvePromise(state) {
+  return next => action => {
+    const key = Object.keys(action).find(k => typeof k.then === 'function');
+    if (key.length) return Promise.resolve(action[key]).then(next, warn);
+    return next(action);
+  };
+}
 
 export default function configureStore(onCompletion) {
 	const enhancer = compose(
-		applyMiddleware(thunk, promise)
+		applyMiddleware(thunk, resolvePromise)
 	);
 
 	const store = createStore(reducer, enhancer);
