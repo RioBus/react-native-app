f996592c69f9bb644eb94335e2edcd5dd89324a4
Fred Souza <fredericoamsouza@gmail.com>
2016-12-30 01:01:38 -0200

    Changing absolute path import position

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from './reducers';
 import { persistStore } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
+import reducer from './reducers';
 
 export default function configureStore(onCompletion) {
 	const store = createStore(reducer, compose(applyMiddleware(thunk)));
