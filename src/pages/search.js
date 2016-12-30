91327ff67a6dd43296b50edc3a26f8ddb8897a29
Fred Souza <fredericoamsouza@gmail.com>
2016-12-30 01:36:51 -0200

    Adding search bar

--- src/pages/search.js
+++ src/pages/search.js
@@ -3,7 +3,8 @@ import { ListView } from 'react-native';
 import {
     Container, Header, Content,
     Title, Button, Icon,
-    ListItem, Spinner
+    ListItem, Spinner, InputGroup,
+    Input
 } from 'native-base';
 
 import { connect } from 'react-redux';
@@ -14,6 +15,8 @@ import { LineItem } from '../components';
 
 class Search extends Component {
 
+    state = { toggleSearch: false, text: '' };
+
     componentWillMount() {
         this.props.downloadLines();
     }
@@ -21,6 +24,35 @@ class Search extends Component {
     get dataSource() {
         const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1.id !== r2.id });
         return ds.cloneWithRows(this.props.lines);
+    }
+
+    renderHeader() {
+        if (this.state.toggleSearch) {
+            return (
+                <Header searchBar rounded>
+                    <InputGroup>
+                        <Icon name="ios-search" />
+                        <Input placeholder="Search" />
+                        <Icon name="ios-close" onPress={() => this.setState({ toggleSearch: false, text: '' })} />
+                    </InputGroup>
+                    <Button transparent>
+                        Search
+                    </Button>
+                </Header>
+            );
+        }
+
+        return (
+            <Header>
+                <Button transparent>
+                    <Icon name='md-menu' onPress={this.props.openDrawer} />
+                </Button>
+                <Title>Rio Bus</Title>
+                <Button transparent onPress={() => this.setState({ toggleSearch: true })}>
+                    <Icon name='md-search' />
+                </Button>
+            </Header>
+        );
     }
 
     renderRow(line) {
@@ -42,15 +74,8 @@ class Search extends Component {
     render() {
         return (
             <Container theme={theme}>
-                <Header>
-                    <Button transparent>
-                        <Icon name='md-menu' onPress={this.props.openDrawer} />
-                    </Button>
-                    <Title>Rio Bus</Title>
-                    <Button transparent>
-                        <Icon name='md-search' onPress={this.props.openDrawer} />
-                    </Button>
-                </Header>
+                {this.renderHeader()}
+
                 <Content>
                     {this.renderContent()}
                 </Content>
