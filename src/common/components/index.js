e6d50a75c215cf2dea48aeb82979a52f2c7cc071
Fred Souza <fredericoamsouza@gmail.com>
2017-01-02 01:49:45 -0200

    Refacotoring

--- src/common/components/index.js
+++
@@ -1,7 +0,0 @@
-import CardView from './CardView';
-import Header from './Header';
-import Icon from './Icon';
-import Searchbar from './Searchbar';
-import Touchable from './Touchable';
-
-export { CardView, Header, Icon, Searchbar, Touchable };
--- src/common/index.js
+++ src/common/index.js
@@ -1,1 +1,7 @@
-export * from './components';
+import CardView from './CardView';
+import Header from './Header';
+import Icon from './Icon';
+import Searchbar from './Searchbar';
+import Touchable from './Touchable';
+
+export { CardView, Header, Icon, Searchbar, Touchable };
